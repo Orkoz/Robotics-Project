9fdda3c9a09554e78ff3ccc1239a632bc0c1c78b
Orkoz <kozlovskyor@gmail.com>
2019-02-19 19:19:50 +0200

    הגנות מסלול ריק

--- Robot_Main.py
+++ Robot_Main.py
@@ -6,12 +6,18 @@ def main():
     create_map()
     print(robot.x, robot.y)
     x, y, yaw_mat = calculate_route(robot.x, robot.y)
+    if not len(x):
+        print('There is no Route')
+        return 0
     # print_arrow_yaw_mat(yaw_mat)
     # print_node_mat()
     initialize_motion(x, y, yaw_mat)
 
     while preform_motion_profile():
         x, y, yaw_mat = calculate_route(robot.x, robot.y)
+        if not len(x):
+            print('There is no Route')
+            return 0
         initialize_motion(x, y, yaw_mat)
 
     # robot.plot_actual_motion()
